bd74c9f9714e2b2ec6df26dfa5bf2ad9ff9ea8e0
Chizitere01 <111280470+Chizitere01@users.noreply.github.com>
2024-06-05 21:55:21 +0100

    First Commit

---
+++ contracts/TokenMaster.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//importing the openzepplin ERC721 token contract
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    uint256 public totalOccasions;
+    uint256 public totalSupply;
+
+    // structs are objects you can give arbitrary parameters
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+    // We use the mapping to add the struct to the function.
+
+    mapping(uint256 => Occasion) occasions;
+    // to prevent the purchase of an nft twice
+    mapping(uint256 => mapping(address => bool)) public hasBought;
+    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+    mapping(uint256 => uint256[]) seatsTaken; //Array of all the seats taken
+
+    // special code to add to a function
+    modifier onlyOwner() {
+        require(msg.sender == owner); // To prevent any other person from creating an event on the platform
+        _;
+    }
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        owner = msg.sender; //The address on the person calling the constructor function.
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner {
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+    // to enable users to buy tickets. we use safemint function from openzepplin library and pass the arguements needed to create the nft.
+    function mint(uint _id, uint256 _seat) public payable {
+        // Require that _id is not 0 or less than total occasions
+        require(_id != 0);
+        require(_id <= totalOccasions);
+
+        //Require that ETH sent is greater than cost...
+        require(msg.value >= occasions[_id].cost);
+
+        //Require that the seat is not taken, and the seat exists...
+        require(seatTaken[_id][_seat] == address(0));
+        require(_seat <= occasions[_id].maxTickets);
+
+        occasions[_id].tickets -= 1; // To update on ticket remaining after buying
+        hasBought[_id][msg.sender] = true; // Update buying status
+        seatTaken[_id][_seat] = msg.sender; //  To assign seat
+        seatsTaken[_id].push(_seat); // To update the seats currently taken
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply); // this will craete an NFT with the id of 1 for the address(buyer)
+    }
+
+    //Reader functions, to read the occasion from the occasion mapping
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
+    }
+
+    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
+        return seatsTaken[_id];
+    }
+
+    //To enable the developer(owner) withdraw the funds from the contract
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+// Components
+import Navigation from "./components/Navigation";
+import Sort from "./components/Sort";
+import Card from "./components/Card";
+import SeatChart from "./components/SeatChart";
+
+// ABIs
+import TokenMaster from "./abis/TokenMaster.json";
+
+// Config
+import config from "./config.json";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+    </div>
+  );
+}
+
+export default App;
