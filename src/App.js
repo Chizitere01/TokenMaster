399d4aa5d486bd5f974a0066af4746e956822e31
Chizitere Okey-Awuzie <chizzit45@gmail.com>
2024-06-06 17:06:07 +0100

    Started the front end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { ethers } from "ethers";
+//import { ethers } from "ethers";
 
-// Components
-import Navigation from "./components/Navigation";
-import Sort from "./components/Sort";
-import Card from "./components/Card";
-import SeatChart from "./components/SeatChart";
+// // Components
+// import Navigation from "./components/Navigation";
+// import Sort from "./components/Sort";
+// import Card from "./components/Card";
+// import SeatChart from "./components/SeatChart";
 
-// ABIs
-import TokenMaster from "./abis/TokenMaster.json";
+// // ABIs
+// import TokenMaster from "./abis/TokenMaster.json";
 
-// Config
-import config from "./config.json";
+// // Config
+// import config from "./config.json";
 
 function App() {
   return (
